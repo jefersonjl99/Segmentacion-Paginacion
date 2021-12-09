eba139e119cf4fb2188ebcb8c2859663d8860973
Elian <brepenaj@correo.udistrital.edu.co>
2021-12-09 15:17:51 -0500

    Primer pull

--- src/segmentacion_paginacion/Segmentacion_Paginacion.java
+++ src/segmentacion_paginacion/Segmentacion_Paginacion.java
@@ -16,6 +16,7 @@ public class Segmentacion_Paginacion {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola mundo");
     }
     
 }
